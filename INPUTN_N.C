9b2b372e89edd1d07e4b0096957c9f36092da58e
Bhumika <bhumika282007@gmail.com>
2025-08-23 15:07:36 +0530

    Add files via upload
    C Program to input 2 numbers from user and print sum of the numbers.

---
+++ INPUTN_N.C
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void main()
+{
+
+int n1;
+int n2;
+int sum;
+
+clrscr();
+
+printf("ENTER NUMBER 1:");
+scanf("%d",&n1);
+
+printf("ENTER NUMBER 2:");
+scanf("%d",&n2);
+
+sum=n1+n2;
+
+printf("SUM OF %d AND %d = %d",n1,n2,sum);
+
+getch();
+
+}
